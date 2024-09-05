3f6c456b079e6d99ade60ca2b13d4130baa0d5e4
francogaray <francogaray.dev@gmail.com>
2024-09-05 18:17:06 -0300

    fix path web link - vite.config file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: 'https://francogaray.github.io/my-portfolio/'
-})
+    plugins: [react()],
+    //base: 'https://francogaray.github.io/my-portfolio/'
+    base: "https://francodev.com.ar",
+});
